e4f14955827ab7c571413cc3f86f23ff78daf6af
Mick Sexton <lacrookedbeat@hotmail.com>
2017-06-09 13:01:24 -0700

    add trash talk statements between Sergei and Milosh, counters, and conditiional statements to execute different strings accordingly

--- js/scripts.js
+++ js/scripts.js
@@ -13,6 +13,21 @@ $(document).ready(function() {
     var ping = 0;
     var pong = 0;
     var pingPong = 0;
+    var sergei1 = "Ready I hope you are for the beating, Milosh.";
+    var milosh1 = "I don't think so, you can't even ping me, Sergei.";
+    var sergei2 = "How you like that, huh? I just ping you right now.";
+    var milosh2 = "Perhaps you ping me once, but you have yet to pong me";
+    var sergei3 = "I pong you now Milosh!";
+    var milosh3 = "Milosh's grandmother could do that, I'd like to see you combine them into the ultimate ping-pong.";
+    var sergei4 = "How you like that Milosh? I just ping-ponged you. Now you must give me all the rubals!";
+    var milosh4 = "Perhaps this young cub has matured into the mama of Russian bears. No matter, Milosh will destroy you like I did my liver long ago";
+    var sergei5 = "Multiple ping-pongs! Sergei is champion, I think, no?";
+    var milosh5 = "Champion of weakling capitalist scum, I'll hack you down like I hacked recent American election!";
+    var sergei6 = "I get ALL the pin-pongs! You cannot deny Sergei's supreme masculinity. Time it is now for you to quit!";
+    var milosh6 = "Well done, you have bested Milosh .... and unfrozen the cold Siberian winter in my heart. Well done!";
+
+    var sergeiSays = "";
+    var miloshSays = "";
 
     for (var i=1; i <= inputNumber; i++) {
     	regularCount[i-1] = i;
@@ -50,6 +65,29 @@ $(document).ready(function() {
       $("ul#reverseOrder").append("<li>" + temp + "</li>");
       //$("#reverseOrder").show();
     }
+
+    if (ping === 0) {
+      sergeiSays = sergei1;
+      miloshSays = milosh1;
+    } else if (ping === 1 && pong === 0) {
+      sergeiSays = sergei2;
+      miloshSays = milosh2;
+    } else if (pong === 1 && pingPong === 0) {
+      sergeiSays = sergei3;
+      miloshSays = milosh3;
+    } else if (pingPong === 1) {
+      sergeiSays = sergei4;
+      miloshSays = milosh4;
+    } else if (pingPong < 3) {
+      sergeiSays = sergei5;
+      miloshSays = milosh5;
+    } else if (pingPong >= 3) {
+      sergeiSays = sergei6;
+      miloshSays = milosh6;
+    }
+
+    $("#sergeiSays").text(sergeiSays);
+    $("#miloshSays").text(miloshSays);
 
     $("#results").show();
     $("#reverseButton").show();
